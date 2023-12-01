bbfd5ba6cfbe0f0bca79e1ab6bfc2ceefad96744
poornika27 <113337276+poornika27@users.noreply.github.com>
2023-12-01 10:47:24 +0530

    Add files via upload

---
+++ day2 3.R
@@ -0,0 +1,9 @@
+A <- c(76, 35, 47, 64, 95, 66, 89, 36, 84)
+> B <- c(51, 56, 84, 60, 59, 70, 63, 66, 50)
+> 
+  > mean_A <- mean(A)
+  > median_A <- median(A)
+  > range_A <- max(A) - min(A)
+  > mean_B <- mean(B)
+  > median_B <- median(B)
+  > range_B <- max(B) - min(B)
